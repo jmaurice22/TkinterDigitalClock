f7dccdecd7194c00b2d59a7b4a67fd9b7ea8156c
jmaurice22 <jpostell29@gmail.com>
2021-07-28 21:44:33 -0700

    modified to 12 hour format

---
+++ clock.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from datetime import datetime
+
+# Canvas to place widgets
+frame = Tk()
+
+# Set canvas title & size
+frame.title('Digital Clcok')
+frame.resizable(1,1)
+
+#Create clock
+clock = Label(frame, font=("Sans-serif", 40, "bold"), fg="red", bg="black")
+clock.grid(row=5, column=3)
+
+#Get time
+def displayClock():
+    localtime = datetime.now()
+    # Format time to 12 hour format
+    time_text = localtime.strftime("%I:%M:%S")
+    clock.configure(text=time_text)
+    clock.after(200, displayClock)
+# Display clock
+displayClock()
+
+# Mainloop
+frame.mainloop()

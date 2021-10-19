266eb2cff83d1a9dac78ae128f22fd16863d9c6f
jpost <jpostell29@gmail.com>
2021-10-18 18:53:22 -0700

    fixed mispelling in app title

--- clock.py
+++ clock.py
@@ -5,20 +5,22 @@ from datetime import datetime
 frame = Tk()
 
 # Set canvas title & size
-frame.title('Digital Clcok')
-frame.resizable(1,1)
+frame.title('Clock')
+frame.resizable(1, 1)
 
-#Create clock
+# Create clock
 clock = Label(frame, font=("Sans-serif", 40, "bold"), fg="red", bg="black")
 clock.grid(row=5, column=3)
 
-#Get time
+# Get time
 def displayClock():
     localtime = datetime.now()
     # Format time to 12 hour format
     time_text = localtime.strftime("%I:%M:%S")
     clock.configure(text=time_text)
     clock.after(200, displayClock)
+
+
 # Display clock
 displayClock()
 
